866e1cfac2c349f13b37521e0bdf873f8867c488
pablo-pc <pablo-362@hotmail.com>
2022-04-06 16:39:32 -0300

    fix db

--- src/api.ts
+++ src/api.ts
@@ -6,7 +6,7 @@ import { Product } from "./types";
 // eslint-disable-next-line import/no-anonymous-default-export
 export default {
   list: async () =>{
-    return axios.get("https://docs.google.com/spreadsheets/d/e/2PACX-1vRs4ZpeuZe7tzaz5yT63EueK-krBqPP54OmPpLDzYLLHdy2GRda5I9YwsujZBpYTIZnBNPhGyxi4Eeg/pub?output=csv", {
+    return axios.get("https://docs.google.com/spreadsheets/d/e/2PACX-1vQWa2wWjKv8Vn4aAchNaISOK9QgyERmbFebnPYLl0V2CoaPPUlTLEusVTz5uILm1NscvkBGo0CRgRB8/pub?output=csv", {
       responseType: "blob"
     }).then(
       response =>{
